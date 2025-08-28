99f2fee7d9c6b4069db2e05e8a3ae036ac230c45
kavinaya-31 <rkavinaya0@gmail.com>
2025-08-28 10:28:37 +0530

    Add files via upload

---
+++ 28.08.25/mypractice.sql
@@ -0,0 +1,5 @@
+select * from student;
+insert into student values('John',23,'cse');
+insert into student values('Praveen',45, 'ece');
+insert into student values('Suresh', 33, 'eee');
+update student 
