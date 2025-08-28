0657f40294e4c25a2c16ae846f5b6af46d6cd163
kavinaya-31 <rkavinaya0@gmail.com>
2025-08-28 10:29:18 +0530

    Delete 28.08.25 directory

--- 28.08.25/mypractice.sql
+++
@@ -1,5 +0,0 @@
-select * from student;
-insert into student values('John',23,'cse');
-insert into student values('Praveen',45, 'ece');
-insert into student values('Suresh', 33, 'eee');
-update student 
