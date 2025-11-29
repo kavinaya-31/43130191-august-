23ead7a725ed0feab7738325269b901d15382b67
kavinaya-31 <rkavinaya0@gmail.com>
2025-11-29 14:37:36 +0530

    Add files via upload

---
+++ 27.11.25/crud.sql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `user` (
+	`sl_no` INT(5) NULL DEFAULT NULL,
+	`name` VARCHAR(100) NULL DEFAULT NULL,
+	`email` VARCHAR(100) NULL DEFAULT NULL
+)
+COLLATE='latin1_swedish_ci'
+ENGINE=InnoDB
+;
